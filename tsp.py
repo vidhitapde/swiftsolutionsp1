d89a1ac52285f7bdfcb8980d14872e8531ea1270
Rachel Chan <rchan2023@gmail.com>
2025-10-22 00:55:08 -0700

    fix data

--- tsp.py
+++ tsp.py
@@ -1,15 +1,17 @@
 import numpy as np
 import pandas as pd
+import math as math
 
 def extract_coords(filename):
-    data = pd.read_csv(filename)
-    data.head()
-    print(data)
+    data_names = ['x', 'y']
+    data = pd.read_csv(filename, sep='\s+', skip_blank_lines=True)
+    data.info(verbose=True)
+    return data
 
 def main():
     print('ComputeDronePath\n')
     filename = input('Enter the name of file: ')
-    extract_coords(filename)
+    data = extract_coords(filename)
 
 if __name__ == '__main__':
   main()
