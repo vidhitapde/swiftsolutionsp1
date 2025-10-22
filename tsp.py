6602b3b1bf14465694cabc9603aedb5298c93a4d
Rachel Chan <rchan2023@gmail.com>
2025-10-22 02:15:36 -0700

    create distance matrix function

--- tsp.py
+++ tsp.py
@@ -5,19 +5,25 @@ import math as math
 def extract_coords(filename):
     data_names = ['x', 'y']
     data = pd.read_csv(filename, sep='\s+', names=data_names)
-    print(data)
-    data.info(verbose=True)
     return data
 
 def euclidean_distance(point1, point2):
-   return math.sqrt((point1.iloc[0] - point2.iloc[0])**2 + (point1.iloc[1] - point2.iloc[1])**2)
+    return math.sqrt((point1.iloc[0] - point2.iloc[0])**2 + (point1.iloc[1] - point2.iloc[1])**2)
+
+def create_distance_matrix(data):
+    n = len(data)
+    dist_matrix = np.zeros((n, n))   #N x N matrix of zeros
+   
+    for i in range(n):
+        for j in range(i, n):
+            dist_matrix[i, j] = euclidean_distance(data.iloc[i], data.iloc[j])
 
 def main():
     print('ComputeDronePath\n')
     filename = input('Enter the name of file: ')
     data = extract_coords(filename)
 
-    print(euclidean_distance(data.iloc[0], data.iloc[1]))
+    distance_matrix = create_distance_matrix(data)
 
 if __name__ == '__main__':
   main()
